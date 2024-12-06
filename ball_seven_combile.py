03e6197ae65036fec9b0020e97fd4bdc565912f0
Pannawit Mahacharoensiri <pannawit.m@ku.th>
2024-12-06 11:08:26 +0700

    finish change ball+run ball to OOP the onlything left is make 7 counter and ball run as the same times

--- ball_seven_combile.py
+++ ball_seven_combile.py
@@ -27,18 +27,18 @@ class Ball:
         turtle.circle(self.ball_radius)
         turtle.end_fill()
 
-    def move_ball(self, i):
-        self.x[i] += self.vx[i] * dt
-        self.y[i] += self.vy[i] * dt
+    def move_ball(self,dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
 
-    def update_ball_velocity(self, i):
+    def update_ball_velocity(self):
         # if the ball hits the side walls, reverse the vx velocity
-        if abs(self.x[i]) > (self.canvas_width - self.ball_radius):
-            self.vx[i] = -self.vx[i]
+        if abs(self.x) > (self.canvas_width - self.ball_radius):
+            self.vx = -self.vx
 
         # if the ball hits the ceiling or the floor, reverse the vy velocity
-        if abs(self.y[i]) > (self.canvas_height - self.ball_radius):
-            self.vy[i] = -self.vy[i]
+        if abs(self.y) > (self.canvas_height - self.ball_radius):
+            self.vy = -self.vy
 
 
 class Run_ball:
@@ -60,6 +60,8 @@ class Run_ball:
             ball_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
             self.ball_lists.append(Ball(x, y, vx, vy, ball_color, i))
 
+
+
     def draw_border(self):
         turtle.penup()
         turtle.goto(-self.canvas_width, -self.canvas_height)
@@ -79,17 +81,173 @@ class Run_ball:
             self.draw_border()
             for i in self.ball_lists:
                 i.draw_ball()
-                # i.move_ball(i)
-                # i.ball.update_ball_velocity()
+                i.move_ball(dt)
+                i.update_ball_velocity()
             turtle.update()
 
+"""
+            drawer = turtle.Turtle()
+            seven = Seven(drawer, "red")
+            seven.run()
+"""
 
 
     # turtle.done()
+class Seven:
+    def __init__(self,my_turtle,color):
+        self.my_turtle = my_turtle
+        self.color = color
+        turtle.speed(0)
+        turtle.tracer(0)
+        turtle.hideturtle()
+        turtle.colormode(255)
+        self.my_turtle.color(color)
+        self.my_turtle.penup()
+        self.my_turtle.setheading(0)
+        self.my_turtle.goto(0, 0)
+        self.my_turtle.pensize(10)
 
+    def draw(self, digit):
+        if digit == 0:
+            self.my_turtle.goto(-50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.penup()
+
+        if digit == 1:
+            self.my_turtle.goto(50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.penup()
+
+        if digit == 2:
+            self.my_turtle.goto(-50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.penup()
+
+        if digit == 3:
+            self.my_turtle.goto(-50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(-100)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.left(90)
+            self.my_turtle.penup()
+
+        if digit == 4:
+            self.my_turtle.goto(-50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(-100)
+            self.my_turtle.forward(-100)
+            self.my_turtle.right(90)
+            self.my_turtle.penup()
+
+        if digit == 5:
+            self.my_turtle.goto(-50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(-100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.left(90)
+            self.my_turtle.penup()
+
+        if digit == 6:
+            self.draw( 5)
+            self.my_turtle.goto(-50, 0)
+            self.my_turtle.pendown()
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.penup()
+
+        if digit == 7:
+            self.my_turtle.goto(-50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.forward(100)
+            self.my_turtle.forward(-100)
+            self.draw(1)
+
+        if digit == 8:
+            self.draw( 0)
+            self.my_turtle.goto(-50, 0)
+            self.my_turtle.pendown()
+            self.my_turtle.forward(100)
+            self.my_turtle.penup()
+
+        if digit == 9:
+            self.draw( 5)
+            self.my_turtle.goto(50, 100)
+            self.my_turtle.pendown()
+            self.my_turtle.right(90)
+            self.my_turtle.forward(100)
+            self.my_turtle.left(90)
+            self.my_turtle.penup()
+
+    def clear(self):
+        self.my_turtle.clear()
+
+    @staticmethod
+    def my_delay(dt):
+        import time
+        start = time.time()
+        while time.time() - start < dt:
+            pass
+
+    def run(self):
+        delay_in_seconds = 0.2
+        while True:
+            for i in range(0, 10):
+                self.clear()
+                self.draw( i)
+                self.my_delay(delay_in_seconds)
+                turtle.update()
 
 Test = Run_ball(7)
 Test.run()
+
 # turtle.colormode(255)
 
 
